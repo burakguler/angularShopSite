66ea694c8591aa91265ad871dc238fa991f35dbc
Burak GÜLER <burak64303@ogr.duzce.edu.tr>
2019-07-21 01:27:02 +0300

    adding some list array for product.component.ts

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
+  title = 'Product List ';
+  products: any[] = [
+    {id: 1, name: 'Laptop', price: 2500, categoryId: 1, description: 'Monster Abra A5'},
+    {id: 2, name: 'Smart Phone', price: 25, categoryId: 2, description: 'Huawei Mate 20 Lite'}
 
+  ];
   ngOnInit() {
   }
 
