d37632a6f85a2976070763aa666445c3ce80097d
Burak GÜLER <burak64303@ogr.duzce.edu.tr>
2019-07-21 03:27:35 +0300

    adding products property and some image urls

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from './product';
 
 @Component({
   selector: 'app-product',
@@ -9,13 +10,13 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title = 'Product List ';
-  products: any[] = [
-    {id: 1, name: 'Laptop', price: 2500, categoryId: 1, description: 'Monster Abra A5'},
-    {id: 2, name: 'Smart Phone', price: 25, categoryId: 2, description: 'Huawei Mate 20 Lite'},
-    {id: 3, name: 'Laptop', price: 2500, categoryId: 1, description: 'Monster Abra A5'},
-    {id: 4, name: 'Smart Phone', price: 25, categoryId: 2, description: 'Huawei Mate 20 Lite'},
-    {id: 5, name: 'Laptop', price: 2500, categoryId: 1, description: 'Monster Abra A5'},
-    {id: 6, name: 'Smart Phone', price: 25, categoryId: 2, description: 'Huawei Mate 20 Lite'}
+  products: Product[] = [
+    {id: 1, name: 'Laptop', price: 2500, categoryId: 1, description: 'Monster Abra A5', imageUrl: 'https://www.monsternotebook.com.tr/UPLOAD/urun-gorselleri-yeni/ABRA/A5_V16/thumb/01-a5_v14-GTX1050_small.png'},
+    {id: 2, name: 'Smart Phone', price: 25, categoryId: 2, description: 'Huawei Mate 20 Lite', imageUrl: 'https://cdn.akakce.com/huawei/huawei-mate-20-lite-64gb-z.jpg'},
+    {id: 3, name: 'Laptop', price: 2500, categoryId: 1, description: 'Monster Abra A7', imageUrl: 'https://www.monsternotebook.com.tr/UPLOAD/urun-gorselleri-yeni/ABRA/A7_V11/thumb/01-A7_V11_small.png'},
+    {id: 4, name: 'Smart Phone', price: 25, categoryId: 2, description: 'Huawei Mate 20 Pro', imageUrl: 'https://cdn.akakce.com/huawei/huawei-mate-20-pro-128gb-z.jpg'},
+    {id: 5, name: 'Laptop', price: 2500, categoryId: 1, description: 'Monster Tulpar T5', imageUrl: 'https://www.monsternotebook.com.tr/UPLOAD/urun-gorselleri-yeni/TULPAR/T5_V19/thumb/01-T5_V19-1650ti_small.png'},
+    {id: 6, name: 'Smart Phone', price: 25, categoryId: 2, description: 'Samsung S10', imageUrl: 'https://cdn.akakce.com/samsung/samsung-galaxy-s10-plus-128gb-beyaz-z.jpg'}
   ];
   ngOnInit() {
   }
