b2b461f98ed01e1714ea908d31a2611ce1fb827d
Burak GÜLER <burak64303@ogr.duzce.edu.tr>
2019-07-21 04:45:29 +0300

    Adding category list

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class CategoryComponent implements OnInit {
 
   constructor() { }
+  title = 'Category List'
+  categories: category[] = [
+    {id: 1, name: 'Smart Phone'},
+    {id: 2, name: 'Computer'},
+    {id: 3, name: 'Accessories'}
+  ]
 
   ngOnInit() {
   }
